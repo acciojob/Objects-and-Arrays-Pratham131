e798833ce98716d3a3077272329cca2efa645627
Pratham131 <104488785+Pratham131@users.noreply.github.com>
2023-06-13 11:56:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 let team = players;
-let team1 = players;
+players.pop();
+let team1 = players.push("na");
 
 person.name = "Accio Job"
 person.age = 25
