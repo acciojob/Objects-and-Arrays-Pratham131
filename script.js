b3dc02be701aaad5648c825a15a348ba1fce76d4
Pratham131 <104488785+Pratham131@users.noreply.github.com>
2023-06-20 09:45:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,14 +7,17 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 let team = players;
-let team1 = players;
-team1.pop();
-team1.unshift();
+let team1 = [...players];
 
-let cap1 = person;
+// team1.pop();
+// team1.unshift();
 
-cap1.name = ""
-cap1.age = 28
+let cap1 = {...person} 
+	
+// 	person;
+
+// cap1.name = ""
+// cap1.age = 28
 
 
 window.players = players;
